bad0ac2cac0a88ae601ec741c6d99cc9aa6088de
yuuy83 <nghiawapro2@gmail.com>
2024-06-16 21:02:29 +0700

    1st upload

---
+++ BookShoppingCartMvcUI/Data/DbSeeder.cs
@@ -0,0 +1,31 @@
+﻿using BookShoppingCartMvcUI.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookShoppingCartMvcUI.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedDefaultData(IServiceProvider service)
+        {
+            var userMgr = service.GetService<UserManager<IdentityUser>>();
+            var roleMgr = service.GetService<RoleManager<IdentityRole>>();
+
+            await roleMgr.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleMgr.CreateAsync(new IdentityRole(Roles.Users.ToString()));
+
+            var admin = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var userInDb = await userMgr.FindByEmailAsync(admin.Email);
+            if (userInDb is null)
+            {
+                await userMgr.CreateAsync(admin,"Admin@123");
+                await userMgr.AddToRoleAsync(admin,Roles.Admin.ToString());
+            }
+        }
+    }
+}
