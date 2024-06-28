0113de55d038376af9e3ae4681336e2c455ac662
yuuy83 <nghiawapro2@gmail.com>
2024-06-28 21:05:23 +0700

    check

--- BookShoppingCartMvcUI/obj/Debug/net8.0/BookShoppingCartMvcUI.AssemblyInfo.cs
+++ BookShoppingCartMvcUI/obj/Debug/net8.0/BookShoppingCartMvcUI.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShoppingCartMvcUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a619012bfa5aefe935cef0d12cdad23aa07d45c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1836b8885a2757f986139eb73b39a33eb8668e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShoppingCartMvcUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShoppingCartMvcUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
